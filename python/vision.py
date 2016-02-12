00359e362ffd112cbb6ce5caa56950f5877d0906
Simon Andrews <simon.andrews212@gmail.com>
2016-02-11 19:25:25 -0500

    Add untested functions for networktables

--- python/vision.py
+++ python/vision.py
@@ -9,7 +9,7 @@ import picamera.array
 from networktables import NetworkTable
 import time
 
-logging.basicConfig(level=logging.CRITICAL)
+logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger()
 
 log.debug("Initialized logger...")
@@ -18,41 +18,65 @@ log.debug("Initialized logger...")
 lower_bound = numpy.array([0,30,105])
 upper_bound = numpy.array([104,255,255])
 
-# networktables setup
-NetworkTable.setIPAddress("roborio-4761-frc.local")
-NetworkTable.setClientMode()
-NetworkTable.initialize()
-table = NetworkTable.getTable("vision")
-log.info("Initialized NetworkTables")
+table = None
+using_networktables = False
+
+def _init_networktables():
+	if not using_networktables:
+		raise Exception("Attempted to initialize NetworkTables while NetworkTables is disabled!")
+	NetworkTable.setIPAddress("roborio-4761-frc.local")
+	NetworkTable.setClientMode()
+	NetworkTable.initialize()
+	table = NetworkTable.getTable("vision")
+	log.info("Initialized NetworkTables")
+
+def write_to_networktables(data):
+	if table is None:
+		init_networktables()
+	try:
+		if not table.isConnected():
+			log.warning("Not connected to a server. Hmmm... (not actually writing anything)")
+		table.putNumber("r_tl_x", data["r_tl_x"])
+		table.putNumber("r_tl_y", data["r_tl_y"])
+		table.putNumber("r_width", data["r_width"])
+		table.putNumber("r_height", data["r_height"])
+		table.putNumber("c_center_x", data["c_center_x"])
+		table.putNumber("c_center_y", data["c_center_y"])
+		table.putNumber("c_radius", data["c_radius"])
+		log.info("Loaded data into NetworkTables")
+	except KeyError:
+		log.exception("Something in NetworkTables didn't work, see stacktrace for details")
 
 with picamera.PiCamera() as camera:
 	camera.framerate = 16
 	camera.shutter_speed = 7000
 	log.info("Initialized camera")
 	count = 0
-	max = 10
+	max_frames = 10
 	stream = io.BytesIO()
-	start = time.time()
 	for foo in camera.capture_continuous(stream, format="jpeg", use_video_port=True):
-		log.info(str(time.time() - start) + "Captured image. Starting to process...")
+		log.info("Captured image. Starting to process...")
 		
 		stream.truncate()
 		stream.seek(0)
 		data = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
 		frame = cv2.imdecode(data, 1)
-		log.debug(str(time.time() - start) + "Converted data to array")
+		log.debug("Converted data to array")
+		cv2.imshow("Original", frame)
 		
 		# Convert BGR to HSV
 		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-		log.debug(str(time.time() - start) + "Converted BGR data to HSV")
+		log.debug("Converted BGR data to HSV")
+		cv2.imshow("BGR to HSV", frame)
 		
 		# Threshold the HSV image
 		mask = cv2.inRange(hsv, lower_bound, upper_bound)
-		log.debug(str(time.time() - start) + "Performed thresholding operation")
+		log.debug("Performed thresholding operation")
+		cv2.imshow("Mask")
 		
 		ret, thresh = cv2.threshold(mask, 127, 255, 0)
 		contours, hierarchy = cv2.findContours(thresh, 3, 2)
-		log.debug(str(time.time() - start) + "Discovered contours")
+		log.debug("Discovered contours")
 		
 		largest_contour = None
 		largest_contour_length = None
@@ -63,26 +87,23 @@ with picamera.PiCamera() as camera:
 			if current_contour_length > largest_contour_length:
 				largest_contour = contour_poly
 				largest_contour_length = current_contour_length
-		log.debug(str(time.time() - start) + "Filtered contours")
+		log.debug("Filtered contours")
 		
 		r_tl_x, r_tl_y, r_width, r_height = cv2.boundingRect(largest_contour)
 		c_center, c_radius = cv2.minEnclosingCircle(largest_contour)
-		log.debug(str(time.time() - start) + "Calculated bounding shapes")
-
-		try:
-			if not table.isConnected():
-				log.warning("Not connected to a server. Hmmm... (not actually writing anything)")
-			table.putNumber("r_tl_x", r_tl_x)
-			table.putNumber("r_tl_y", r_tl_y)
-			table.putNumber("r_width", r_width)
-			table.putNumber("r_height", r_height)
-			table.putNumber("c_center_x", c_center[0])
-			table.putNumber("c_center_y", c_center[1])
-			table.putNumber("c_radius", c_radius)
-			log.info("Loaded data into NetworkTables")
-		except KeyError:
-			log.exception("Something in NetworkTables didn't work, see stacktrace for details")
+		log.debug("Calculated bounding shapes")
+		
+		if using_networktables:
+			data = {}
+			data["r_tl_x"] = r_tl_x
+			data["r_tl_y"] = r_tl_y
+			data["r_width"] = r_width
+			data["r_height"] = r_height
+			data["c_center_x"] = c_center[0]
+			data["c_center_y"] = c_center[1]
+			data["c_radius"] = c_radius
+			write_to_networktables(data)
 
 		count += 1
-		if count >= max:
+		if count >= max_frames:
 			break

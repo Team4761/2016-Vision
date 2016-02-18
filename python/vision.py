e361e22605f0857555a20c28dadd5d1f686d9aa5
Simon Andrews <simon.andrews212@gmail.com>
2016-02-18 18:45:19 -0500

    Tuning + distances + command line arguments + error catching

--- python/vision.py
+++ python/vision.py
@@ -6,6 +6,7 @@ import numpy
 import picamera
 import picamera.array
 from networktables import NetworkTable
+import sys
 import time
 
 logging.basicConfig(level=logging.DEBUG)
@@ -14,7 +15,7 @@ log = logging.getLogger()
 log.debug("Initialized logger...")
 
 # define range of color in BGR
-lower_bound = numpy.array([0,80,0])
+lower_bound = numpy.array([0,110,0])
 upper_bound = numpy.array([255,255,84])
 
 global table
@@ -37,10 +38,10 @@ def write_to_networktables(data):
 		_init_networktables()
 	try:
 		if not table.isConnected():
-			log.warning("Not connected to a server. Hmmm... (not actually writing anything)")
+			log.warning("Not connected to a NetworkTables server (nothing will actually be sent)")
 		for key in data.keys():
 			table.putNumber(key, data[key])
-		log.info("Loaded data into NetworkTables")
+		log.info("Loaded data into NetworkTable")
 	except KeyError:
 		log.exception("Something in NetworkTables didn't work, see stacktrace for details")
 
@@ -49,7 +50,8 @@ with picamera.PiCamera() as camera:
 	camera.shutter_speed = 200
 	log.info("Initialized camera")
 	count = 0
-	max_frames = 200
+	max_frames = int(sys.argv[1])
+	time.sleep(0.5) #Shutter speed is not set instantly. This allows changes to take effect.
 	with picamera.array.PiRGBArray(camera) as stream:
 		for foo in camera.capture_continuous(stream, format="bgr", use_video_port=True):
 			log.info("Captured image. Starting to process...")
@@ -69,10 +71,17 @@ with picamera.PiCamera() as camera:
 			contours, hierarchy = cv2.findContours(thresh, 3, 2)
 			log.debug("Discovered contours")
 			
-			#Remember, if someone offers you functional programming just say NO! and run away
-			#These two lines find the largest contour by perimeter in the image
-			contours_poly = [cv2.approxPolyDP(contour, 3, True) for contour in contours]
-			largest_contour = sorted(contours_poly, key=lambda cp: -cv2.arcLength(cp, True))[0]
+			try:
+				#Remember, if someone offers you functional programming just say NO! and run away
+				#These two lines find the largest contour by perimeter in the image
+				contours_poly = [cv2.approxPolyDP(contour, 3, True) for contour in contours]
+				largest_contour = sorted(contours_poly, key=lambda cp: -cv2.arcLength(cp, True))[0]
+			except IndexError:
+				log.error("No contours found. Continuing...")
+				count += 1
+				if count >= max_frames:
+					break
+				continue
 			
 			topleft_x, topleft_y, width, height = cv2.boundingRect(largest_contour)
 			log.debug("Calculated bounding shapes")
@@ -80,6 +89,7 @@ with picamera.PiCamera() as camera:
 			print height
 			cv2.rectangle(frame, (topleft_x, topleft_y), (topleft_x + width, topleft_y + height), (255,255,255), 3)
 			cv2.imwrite("modded{}.jpg".format(count), frame)
+			log.info("Wrote image!")
 			
 			if using_networktables:
 				data = {
@@ -88,6 +98,7 @@ with picamera.PiCamera() as camera:
 					"width": width,
 					"height": height,
 					"horiz_offset": (topleft_x + (width / 2)) - (camera.resolution[0] / 2),
+					"distance_guess": -0.28478 * height + 43.143,
 				}
 				write_to_networktables(data)
 			

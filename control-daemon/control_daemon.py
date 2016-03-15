384d91e3426ed24fa3991fc6d759b29e61e92487
Simon Andrews <simon.andrews212@gmail.com>
2016-03-15 19:33:09 -0400

    Remove start vision flag; add systemd service for vision (not working)

--- control-daemon/control_daemon.py
+++ control-daemon/control_daemon.py
@@ -1,6 +1,6 @@
 import logging
 from networktables import NetworkTable
-import os
+import subprocess
 import time
 
 logging.basicConfig(level=logging.DEBUG)
@@ -17,25 +17,21 @@ def reset_flags():
 	log.debug("Setting default values for flags...")
 	table.putBoolean("shutdown_flag", False)
 	table.putBoolean("reboot_flag", False)
-	table.putBoolean("start_vision_flag", False)
 	log.debug("Default flag values set!")
 
 reset_flags()
 
 log.debug("Entering main loop...")
+
 while True:
 	table.putNumber("last_updated", time.time())
 	if table.getBoolean("reboot_flag", False):
 		reset_flags()
 		log.info("Reboot flag is true! Rebooting...")
-		os.system("reboot")
+		subprocess.Popen(["reboot"])
 	if table.getBoolean("shutdown_flag", False):
 		reset_flags()
 		log.info("Shutdown flag is true! Shutting down...")
-		os.system("shutdown -h now")
-	if table.getBoolean("start_vision_flag", False):
-		reset_flags()
-		log.info("Vision flag is true! Starting vision program...")
-		os.system("python 2016-Vision/python/vision.py")
+		subprocess.Popen(["shutdown", "-h", "now"])
 	log.debug("No flags set to true. Sleeping for five seconds...")
 	time.sleep(5)

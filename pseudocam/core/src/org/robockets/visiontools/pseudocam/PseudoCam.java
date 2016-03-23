e625306695c188e2fd930530b86a28b9deae5774
Simon Andrews <simon.andrews212@gmail.com>
2016-03-23 19:19:43 -0400

    Add probably working pseudocam

--- pseudocam/core/src/org/robockets/visiontools/pseudocam/PseudoCam.java
+++ pseudocam/core/src/org/robockets/visiontools/pseudocam/PseudoCam.java
@@ -2,26 +2,60 @@ package org.robockets.visiontools.pseudocam;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class PseudoCam extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private ShapeRenderer shapeRenderer;
+	private float width;
+	private float height;
+
+	private NetworkTable visionTable;
+	private double bbBottomLeftX;
+	private double bbBottomLeftY;
+	private double bbWidth;
+	private double bbHeight;
+	private double canSeeTarget;
 	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
+		shapeRenderer = new ShapeRenderer();
+
+		NetworkTable.setClientMode();
+		NetworkTable.setIPAddress("roborio-4761-frc.local");
+		visionTable = NetworkTable.getTable("vision");
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		refreshNetworkTableValues();
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		if(!visionTable.isConnected()) {
+			shapeRenderer.setColor(Color.YELLOW);
+		}
+		else if(canSeeTarget == 1.0) {
+			shapeRenderer.setColor(Color.GREEN);
+		}
+		else {
+			shapeRenderer.setColor(Color.RED);
+		}
+		shapeRenderer.rect((float) bbBottomLeftX, (float) bbBottomLeftY, (float) bbWidth, (float) bbHeight);
+		shapeRenderer.setColor(Color.WHITE);
+		shapeRenderer.rectLine(width / 2, 0, width / 2, height, 3);
+		shapeRenderer.end();
+	}
+
+	private void refreshNetworkTableValues() {
+		bbWidth = visionTable.getNumber("width", 50);
+		bbHeight = visionTable.getNumber("height", 50);
+		bbBottomLeftX = visionTable.getNumber("topleft_x", 0);
+		bbBottomLeftY = height - visionTable.getNumber("topleft_y", 50);
+		canSeeTarget = visionTable.getNumber("can_see_target", 0);
 	}
 }
--- pseudocam/desktop/src/org/robockets/visiontools/pseudocam/desktop/DesktopLauncher.java
+++ pseudocam/desktop/src/org/robockets/visiontools/pseudocam/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import org.robockets.visiontools.pseudocam.PseudoCam;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = false;
+		config.width = 640;
+		config.height = 480;
 		new LwjglApplication(new PseudoCam(), config);
 	}
 }
